098950c18a792ec3b1428910acb017a9cde360c6
satyanarayanPatra <satya77888narayanpatra@gmail.com>
2025-04-01 07:11:03 +0530

    add changes

--- typescript-course-main/01-typescript-tutorial/src/tutorial.ts
+++ typescript-course-main/01-typescript-tutorial/src/tutorial.ts
@@ -1,5 +1,13 @@
 console.log('typescript tutorial');
 
 interface someValue {
-	
+	name: string;
+	id: number;
 }
+
+const someObject: someValue = {
+	name: 'random',
+	id: 123,
+};
+
+console.log(someObject);
